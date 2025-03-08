9229eadce900cfb26c21a764dc8aff300cc99a9b
joesantos418 <joesantos386@gmail.com>
2025-03-08 13:18:02 -0300

    nova solução

---
+++ 05-tipos-complexos/solucoes/04-dobrando-pares.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := [14]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 20, 30, 31}
+	b := dobraPares(a)
+
+	for _, valor := range b {
+		fmt.Println(valor)
+	}
+}
+
+func dobraPares(a [14]int) [14]int {
+	var b [14]int
+	for chave, valor := range a {
+		if valor%2 == 0 {
+			b[chave] = valor * 2
+		} else {
+			b[chave] = valor
+		}
+	}
+
+	return b
+}
